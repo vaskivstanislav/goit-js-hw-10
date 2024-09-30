878579a96044c2841380c15fcde3af01d4dc084e
vaskivstanislav <stasmegatyr@gmail.com>
2024-09-30 15:29:09 +0300

    test-3

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/goit-js-hw-10/',
+    base: '/Goit-js-hw-10/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
